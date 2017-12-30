48738edc5791b532cb9ec5f096a2ba009ed9a977
Olle Fredriksson <fredriksson.olle@gmail.com>
2017-12-30 23:54:14 +0100

    Add Text.Parsix module with reexports

---
+++ src/Text/Parsix.hs
@@ -0,0 +1,10 @@
+module Text.Parsix(module X) where
+
+import Text.Parser.Char as X
+import Text.Parser.Combinators as X
+import Text.Parser.Token as X
+
+import Text.Parsix.Combinators as X
+import Text.Parsix.Parser as X
+import Text.Parsix.Position as X
+import Text.Parsix.Result as X
